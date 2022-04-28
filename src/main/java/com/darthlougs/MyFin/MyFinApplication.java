824fd1a24d51919ab2cd2b381839e9f11da08141
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-04-28 09:15:38 -0300

    Inicio projeto MyFin

---
+++ src/main/java/com/darthlougs/MyFin/MyFinApplication.java
@@ -0,0 +1,13 @@
+package com.darthlougs.MyFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darthlougs/MyFin/MyFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darthlougs.MyFin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
