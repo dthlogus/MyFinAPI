26e25c4a412ad3fc068524c2817c79d1b85182d3
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-04-29 07:47:58 -0300

    Implementação dos métodos Usuario service

--- src/main/java/com/darthlougs/MyFin/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/darthlougs/MyFin/service/impl/UsuarioServiceImpl.java
@@ -1,11 +1,15 @@
 package com.darthlougs.MyFin.service.impl;
 
 import com.darthlougs.MyFin.entity.Usuario;
+import com.darthlougs.MyFin.exception.ExceptionAutenticacao;
 import com.darthlougs.MyFin.exception.RegraNegocioException;
 import com.darthlougs.MyFin.repository.UsuarioRepository;
 import com.darthlougs.MyFin.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -15,12 +19,24 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public Usuario autenticar(String email, String senha) {
-        return null;
+        Optional<Usuario> usuario = repository.findByEmail(email);
+
+        if (!usuario.isPresent()){
+            throw new ExceptionAutenticacao("Usuário não encontrado para o e-mail informado");
+        }
+
+        if (usuario.get().getSenha().equals(senha)){
+            throw new ExceptionAutenticacao("Senha inválida");
+        }
+
+        return usuario.get();
     }
 
     @Override
+    @Transactional
     public Usuario salvarUsuario(Usuario usuario) {
-        return null;
+        validarEmail(usuario.getEmail());
+        return repository.save(usuario);
     }
 
     @Override
